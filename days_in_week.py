d67be75052957daa6ba47ede9843dc1be6c53f33
Jason2612 <phongnam1234523@gmail.com>
2022-04-26 18:16:05 +0700

    first file commit

---
+++ days_in_week.py
@@ -0,0 +1,18 @@
+num_input = int(input("Type in a number (from 1 to 7): "))
+
+if num_input == 1:
+    print("Monday")
+elif num_input == 2:
+    print("Tuesday")
+elif num_input == 3:
+    print("Wednesday")
+elif num_input == 4:
+    print("Thursday")
+elif num_input == 5:
+    print("Friday")
+elif num_input == 6:
+    print("Saturday")
+elif num_input == 7:
+    print("Sunday")
+else:
+    print("Error, out of the range")
